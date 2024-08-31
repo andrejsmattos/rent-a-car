342e74ff546c847d6c39598e6d4f545720c0076e
André <andrejusmattos@gmail.com>
2024-08-31 14:02:08 -0300

    confirguração de roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ListCarsComponent } from './pages/list-cars/list-cars.component';
+import { DetailCarComponent } from './pages/detail-car/detail-car.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', redirectTo: 'lista', pathMatch: 'full' },
+  { path: 'lista', component: ListCarsComponent },
+  { path: 'lista/:id', component: DetailCarComponent },
+
+];
