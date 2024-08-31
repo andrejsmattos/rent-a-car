053eda892cd744133243850d957d25f77a76e6f0
André <andrejusmattos@gmail.com>
2024-08-31 13:52:56 -0300

    estrutura do projeto

---
+++ src/app/pages/detail-car/detail-car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-car',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-car.component.html',
+  styleUrl: './detail-car.component.scss'
+})
+export class DetailCarComponent {
+
+}
---
+++ src/app/pages/list-cars/list-cars.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-cars',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-cars.component.html',
+  styleUrl: './list-cars.component.scss'
+})
+export class ListCarsComponent {
+
+}
